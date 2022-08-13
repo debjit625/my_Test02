009f16d468aa449f33a62373b637ed20119e4df4
Debjit Roy <deb.jeet.625@gmail.com>
2022-08-13 14:28:47 +0530

    Added cal_pro function in app.h and called in main.c

---
+++ app.h
@@ -0,0 +1,4 @@
+void cal_pro(double a,double b)
+{
+return a*b;
+}
--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio>
+#include  "app.h"
 
 int mani()
 {
+double result = cal_pro(4*5);
 printf("Hello world!");
 return 0;
 }
